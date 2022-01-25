9edc5e62c5c4d89664aeaba6c818f95b38aa35de
ashish <ashu63852@gmail.com>
2022-01-25 14:07:09 +0530

    just checking

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum{
+
+    public static void main(String args[])
+    {
+    System.out.println(add(6,7));
+    }
+    private static int add(int a, int b) {
+        return (a+b);
+    }
+}
