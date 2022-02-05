be831938115ee9094773b95e3cfc9e34c4d586ec
ashish <ashishsinghh40@gmail.com>
2022-02-05 12:53:19 +0530

    2 methods
    3 methods now
    3 methods now
    3 methods now

--- sum.java
+++ sum.java
@@ -3,6 +3,7 @@ public class sum{
     public static void main(String args[])
     {
     System.out.println(add(6,7));
+    System.out.println(add(6,7));
     }
     private static int add(int a, int b) {
         return (a+b);
